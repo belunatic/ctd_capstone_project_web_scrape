6df7f889e234ec1ae6d057c3b5306390115b0a41
belunatic <abelluvyou@yahoo.com>
2026-09-11 16:03:13 -0700

    add a sidebar, selection box and sample metrics

--- weather_streamlit_app.py
+++ weather_streamlit_app.py
@@ -14,6 +14,7 @@ FROM weather w
 JOIN continent c ON w.continent_id = c.id
 """
 
+#data frame from sql query
 df = pd.read_sql_query(sql_statement, conn)
 
 #filter the data frame by continent
@@ -23,7 +24,10 @@ df_north_america = df[df['Continent'] == 'North America']
 df_south_america = df[df['Continent'] == 'South America']
 
 #get a list of unique continents from the data frame
-continents = df['Continent'].unique().tolist()
+continents_list = df['Continent'].unique().tolist()
+#add another option to the list of continents for all continents
+continents_list.append('All Continents')
+continents_list.sort()
 
 #since the collected date and time are the same for all rows, we can get the first row's collected date and time
 collected_date = df['Collected_Date'].iloc[0]
@@ -37,6 +41,15 @@ collected_time = pd.to_datetime(collected_time).strftime('%I:%M %p')
 page_title = "Weather Data As of " + collected_date + " at " + collected_time;
 st.set_page_config(page_title=page_title, page_icon="🌞", layout="wide")
 
+
+#sidebar
+st.sidebar.title('Explore')
+sidebar_option = st.sidebar.selectbox('Select Continent', continents_list)
+
 #Main Content
 st.title("Weather For Popular Cities")
 st.markdown('This data was collected from *timeanddate.com* on ' + collected_date + ' at ' + collected_time + ' (local time).')
+
+#display for 'All continent' VS 'A continent'
+if sidebar_option == 'All Continents':
+    st.metric(f"${df['Country'].values[0]}", f"{df['Weather'].values[0]} F")
